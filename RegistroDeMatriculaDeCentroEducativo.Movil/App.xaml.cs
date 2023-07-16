17dce1949f8035513c5f9e237e80be489ee40054
Francisco Castillo <francisco.castillosalazar@ucr.ac.cr>
2023-07-16 12:13:29 -0600

    Aplicacion Movil

---
+++ RegistroDeMatriculaDeCentroEducativo.Movil/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistroDeMatriculaDeCentroEducativo.Movil
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
