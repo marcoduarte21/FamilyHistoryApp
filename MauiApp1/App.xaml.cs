fafbc53e00a908f511690b0ffb7a2faf070824e4
marcoduarte21 <marco.duarte@ucr.ac.cr>
2023-07-16 10:50:36 -0600

    agregación del proyecto movil

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using RegistroDeMatriculaDeCentroEducativo.Movil;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+          MainPage = new NavigationPage(new VistaEstudiantes());
+        }
+    }
+}
