5e19b3e92cb06e5a7aade1d38ba8d2f721c278d7
marcoduarte21 <marco.duarte@ucr.ac.cr>
2025-05-30 19:52:22 -0600

    removing some unused folders

--- RegistroDeMatriculaDeCentroEducativo.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RegistroDeMatriculaDeCentroEducativo.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
